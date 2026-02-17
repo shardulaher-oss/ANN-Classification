20bb6162e5cdc782541ec85a3db9a7500d4cbc27
shardulaher-oss <shardulaher@gmail.com>
2026-02-18 03:39:48 +0530

    regression

---
+++ regression.py
@@ -0,0 +1,56 @@
+import tensorflow as tf 
+import pandas as pd
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler,OneHotEncoder,LabelEncoder
+import pickle
+
+model=tf.keras.models.load_model('regression_model.h5')
+with open('label_encoder_reg.pkl','rb') as file:
+    label_encoder_gender=pickle.load(file)
+with open('onehot_encoder_reg.pkl','rb') as file:
+    one_hot_encoder_geo=pickle.load(file)
+with open('scaler_reg.pkl','rb') as file:
+    scaler=pickle.load(file)
+
+#Streamlit app
+
+st.title('Customer Salary Prediction')
+
+#user input
+
+geography=st.selectbox('Geography',one_hot_encoder_geo.categories_[0])
+gender=st.selectbox('Gender',label_encoder_gender.classes_)
+age=st.slider('Age',18,90)
+balance=st.number_input('Balance')
+credit_Score=st.number_input('CreditScore')
+tenure=st.slider('Tenure',0,10)
+num_of_products=st.slider('NumberOfProducts',1,4)
+has_cr_card=st.selectbox("HasCreditCard",[0,1])
+is_active_memeber=st.selectbox('IsActiveMember',[0,1])
+exited=st.selectbox('Exited',[0,1])
+
+# Build input DataFrame using the original dataset feature names
+input_data = pd.DataFrame({
+    'CreditScore': [credit_Score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_memeber],
+    'Exited':[exited]
+})
+
+# One-hot encode geography and concat (columns will be like 'Geography_France')
+geo_encoded = one_hot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=one_hot_encoder_geo.get_feature_names_out(['Geography']))
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+input_data_scaled=scaler.transform(input_data)
+
+salary=model.predict(input_data_scaled)
+
+st.write(f"Predicted Salary: {salary[0][0]}")
+
